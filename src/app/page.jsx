72996bc6e3ed1f9dd731e08e8e8606f2fccafc24
NeetuSharma3012 <riyasharma16602@gmail.com>
2024-11-19 23:22:32 +0530

    frontend

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,14 +14,14 @@ const Home = () => {
     <button className='global_btn'>home button</button>
     <img src="/globe.svg" className='size-16' alt="" />
 
-    <div className='m-3 w-7/8 '>
+    <div className='flex flex-wrap m-3 w-7/8 '>
     <Card title="card title 1" description="card 1 description"/>
     <Card title="card title 2" description="card 2 description"/>
     <Card title="card title 3" description="card 3 description"/>
     <Card title="card title 4" description="card 4 description"/>
+    </div>
+    <div>
     <MyButton disabled={true}>submit</MyButton>
-    
-
     </div>
     </div>
 
